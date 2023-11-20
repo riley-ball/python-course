396b3e8ecbda7795f30754807bff7f10d579be67
Riley Ball <r.ball@uqconnect.edu.au>
2023-11-20 14:52:16 +1000

    added pre-class quiz

---
+++ official/week6/class_quiz.py
@@ -0,0 +1,18 @@
+my_list = ["Riley", 4.20, 24]
+
+my_list[0] = "Billy"
+
+my_list[-1] += 1
+
+# Below is a list of methods that can be used on lists and the parameters they take:
+# append(item)
+# insert(index, item)
+# remove(item)
+# pop(index)
+# clear()
+# index(item)
+# count(item)
+# sort()
+# reverse()
+
+print(my_list.sort())
