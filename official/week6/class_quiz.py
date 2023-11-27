24153fd4a19bf864ce231e76c017a3deecc7fb49
Riley <riley-ball@outlook.com>
2023-11-27 15:19:45 +1000

    cleanup

--- official/week6/class_quiz.py
+++ official/week6/class_quiz.py
@@ -1,18 +1,20 @@
-my_list = ["Riley", 4.20, 24]
-
-my_list[0] = "Billy"
-
-my_list[-1] += 1
+# Variable, List, String, Float, Integer
+# 
+my_list = [4.20, 24, 24, 24]
+# Grabbing the 0th index, replacing it with "Billy"
+# my_list[0] = "Billy"
+# 
+my_list[-1] += 2
 
 # Below is a list of methods that can be used on lists and the parameters they take:
-# append(item)
-# insert(index, item)
-# remove(item)
-# pop(index)
-# clear()
-# index(item)
-# count(item)
+# append(item) - adds an item to the end of the list
+# insert(index, item) - adds an item to the specified index
+# remove(item) - removes the first occurrence of the item
+# pop(index) - removes the item at the specified index and returns it
+# clear() - removes all items from the list
+# count(item) - returns the number of occurrences of the item
 # sort()
 # reverse()
 
-print(my_list.sort())
+print(my_list.reverse())
+print(my_list)
